b197907e469bbb22947fe820006a152b79b33db6
richelbilderbeek <richel@richelbilderbeek.nl>
2023-05-08 16:04:04 +0200

    Have two algorithms

--- my_functions.py
+++ my_functions.py
@@ -3,6 +3,7 @@ def add_one(x):
 
 from collections import deque
 from functools import partial
+from itertools import product
 
 def align_bf(x, y):
     """Align two sequences, maximizing the
@@ -14,6 +15,16 @@ def align_bf(x, y):
         all_alignments(x, y),
         key=partial(alignment_score, x, y),
     )
+
+def align_fast(x, y):
+    """Align two sequences, maximizing the
+    alignment score, using the Needleman-Wunsch
+    algorithm.
+
+    x, y -- sequences.
+    """
+    return needleman_wunsch(x, y)
+
 
 def alignment_score(x, y, alignment):
     """Score an alignment.
@@ -71,3 +82,73 @@ def all_alignments(x, y):
 
     alignments = F(range(len(x)), range(len(y)))
     return map(list, alignments)
+
+
+def needleman_wunsch(x, y):
+    """Run the Needleman-Wunsch algorithm on two sequences.
+
+    x, y -- sequences.
+
+    Code based on pseudocode in Section 3 of:
+
+    Naveed, Tahir; Siddiqui, Imitaz Saeed; Ahmed, Shaftab.
+    "Parallel Needleman-Wunsch Algorithm for Grid." n.d.
+    https://upload.wikimedia.org/wikipedia/en/c/c4/ParallelNeedlemanAlgorithm.pdf
+    """
+    N, M = len(x), len(y)
+    s = lambda a, b: int(a == b)
+
+    DIAG = -1, -1
+    LEFT = -1, 0
+    UP = 0, -1
+
+    # Create tables F and Ptr
+    F = {}
+    Ptr = {}
+
+    F[-1, -1] = 0
+    for i in range(N):
+        F[i, -1] = -i
+    for j in range(M):
+        F[-1, j] = -j
+
+    option_Ptr = DIAG, LEFT, UP
+    for i, j in product(range(N), range(M)):
+        option_F = (
+            F[i - 1, j - 1] + s(x[i], y[j]),
+            F[i - 1, j] - 1,
+            F[i, j - 1] - 1,
+        )
+        F[i, j], Ptr[i, j] = max(zip(option_F, option_Ptr))
+
+    # Work backwards from (N - 1, M - 1) to (0, 0)
+    # to find the best alignment.
+    alignment = deque()
+    i, j = N - 1, M - 1
+    while i >= 0 and j >= 0:
+        direction = Ptr[i, j]
+        if direction == DIAG:
+            element = i, j
+        elif direction == LEFT:
+            element = i, None
+        elif direction == UP:
+            element = None, j
+        alignment.appendleft(element)
+        di, dj = direction
+        i, j = i + di, j + dj
+    while i >= 0:
+        alignment.appendleft((i, None))
+        i -= 1
+    while j >= 0:
+        alignment.appendleft((None, j))
+        j -= 1
+
+    return list(alignment)
+
+def print_alignment(x, y, alignment):
+    print("".join(
+        "-" if i is None else x[i] for i, _ in alignment
+    ))
+    print("".join(
+        "-" if j is None else y[j] for _, j in alignment
+    ))
