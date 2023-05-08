3d0afa588901ba16ce3307bd9da91fec85e00eeb
richelbilderbeek <richel@richelbilderbeek.nl>
2023-05-08 15:48:19 +0200

    Add code from https://johnlekberg.com/blog/2020-10-25-seq-align.html

--- my_functions.py
+++ my_functions.py
@@ -1,2 +1,40 @@
 def add_one(x):
     return x + 1
+
+from collections import deque
+
+def all_alignments(x, y):
+    """Return an iterable of all alignments of two
+    sequences.
+
+    x, y -- Sequences.
+    """
+
+    def F(x, y):
+        """A helper function that recursively builds the
+        alignments.
+
+        x, y -- Sequence indices for the original x and y.
+        """
+        if len(x) == 0 and len(y) == 0:
+            yield deque()
+
+        scenarios = []
+        if len(x) > 0 and len(y) > 0:
+            scenarios.append((x[0], x[1:], y[0], y[1:]))
+        if len(x) > 0:
+            scenarios.append((x[0], x[1:], None, y))
+        if len(y) > 0:
+            scenarios.append((None, x, y[0], y[1:]))
+
+        # NOTE: "xh" and "xt" stand for "x-head" and "x-tail",
+        # with "head" being the front of the sequence, and
+        # "tail" being the rest of the sequence. Similarly for
+        # "yh" and "yt".
+        for xh, xt, yh, yt in scenarios:
+            for alignment in F(xt, yt):
+                alignment.appendleft((xh, yh))
+                yield alignment
+
+    alignments = F(range(len(x)), range(len(y)))
+    return map(list, alignments)
